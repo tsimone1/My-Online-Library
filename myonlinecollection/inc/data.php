de3c6870ea629e8fc617ff350dd58dacd0bd4365
tsimone1 <34288548+tsimone1@users.noreply.github.com>
2017-12-05 18:48:58 -0500

    Add files via upload

---
+++ myonlinecollection/inc/data.php
@@ -0,0 +1,152 @@
+<?php
+$catalog = [];
+
+Books
+$catalog[101] = [
+	"title" => "Milk & Honey",
+	"img" => "img/media/milkandhoney.jpg",
+    "genre" => "Poetry",
+    "format" => "Paperback",
+    "year" => 2015,
+    "category" => "Books",
+    "authors" => [
+        "Rupi Kaur"
+    ],
+    "publisher" => "Andrews McMeel Publishing; Reprint edition",
+    "isbn" => '978-1449474256'
+    
+    
+    $catalog[102] = [
+	"title" => "My Sister's keeper",
+	"img" => "img/media/mysisterskeeper.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2005,
+    "category" => "Books",
+    "authors" => [
+        "Jodi Picoult"
+    ],
+    "publisher" => "Washington Square Press",
+    "isbn" => '978-0743454537'
+    
+    $catalog[103] = [
+	"title" => "The Angel Experiment: A Maximum Ride Novel",
+	"img" => "img/media/maximumride.jpg",
+    "genre" => "Teen & Young Adult",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "James Patterson"
+    ],
+    "publisher" => "jimmy patterson",
+    "isbn" => '978-0316067959'
+    
+    $catalog[104] = [
+	"title" => "A Walk to Remember",
+	"img" => "img/media/walktoremember.jpg",
+    "genre" => "Drama",
+    "format" => "Paperback",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Nicholas Sparks"
+    ],
+    "publisher" => "Grand Central Publishing",
+    "isbn" => '978-0446693806'
+];
+
+
+
+
+Movies
+$catalog[201] = [
+    "title" => "Forrest Gump",
+    "img" => "img/media/forestgump.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+    "stars" => [
+        "Tom Hanks",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+    
+    
+    $catalog[202] = [
+    "title" => "Death Proof",
+    "img" => "img/media/deathproof.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+    "stars" => [
+        "Tom Hanks",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+    
+    
+    $catalog[203] = [
+    "title" => "Eternal Sunshine of the Spotless Mind",
+    "img" => "img/media/ES.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 2004,
+    "category" => "Movies",
+    "director" => "Michel Gondry",
+    "writers" => [
+        "Charlie Kaufman",
+        "Michel Gondry"
+    ],
+    "stars" => [
+        "Jim Carrey",
+        "Kate Winslet",
+        "Tom Wilkinson",
+    ]
+    
+    
+    $catalog[204] = [
+    "title" => "P.S. I Love You",
+    "img" => "img/media/psily.jpg",
+    "genre" => "Romance",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Richard LaGravenese",
+    "writers" => [
+        "Richard LaGravenese",
+        "Steven Rogers"
+    ],
+    "stars" => [
+        "Hilary Swank",
+        "Gerad Butler",
+        "Harry Connick Jr."
+    ]
+];
+
+
+
+//Music
+$catalog[301] = [
+    "title" => "About Time",
+    "img" => "img/media/abouttime.jpg",
+    "genre" => "R&B/Soul",
+    "format" => "Download",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Sabrina Claudio"
+];
---
+++ myonlinecollection/inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+ 
+    <div class="footer">
+ 
+        <div class="wrapper">
+ 
+            <ul>
+                <li><a href="https://www.instagram.com/LaGuardiaCC/">Twitter</a></li>
+                <li><a href="https://www.facebook.com/LaGuardiaCC">Facebook</a></li>
+            </ul>
+ 
+            <p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+ 
+        </div>
+ 
+    </div>
+ 
+</body>
+</html>
